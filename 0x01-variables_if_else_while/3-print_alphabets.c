d75b8f7cf7c8c15cfa540cd2ff1a71c62ac72901
Wambani01 <georgewmbn@gmail.com>
2022-09-09 11:19:29 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - Entry point
+ *Return: Always 0 (successs)
+ */
+int main(void)
+{
+	char a;
+	char b;
+
+	for (a = 'a'; a <= 'z' a++)
+	{
+		putchar(a);
+	}
+
+	for (b = 'A' b <= 'Z' b++)
+	{
+		putchar(b);
+	}
+	return (0);
+}
