e245d32ea75bcbf3926e251200d3b2dcd5ee28eb
Wambani01 <georgewmbn@gmail.com>
2022-09-09 12:32:21 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	char a;
 
-	for ((a = 'a'; a <= 'd' a++); && (a = 'f'; a <= 'p'; a++); && (a = 'r'; a <= 'z'; a++);
-			{
-				putchar(a);
-			}
+	for ((a = 'a'; a <= 'd' a++); &&(a = 'f'; a <= 'p'; a++); &&(a = 'r'; a <= 'z'; a++));
+	{
+
+		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
