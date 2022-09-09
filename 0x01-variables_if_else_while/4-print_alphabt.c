5d16b12dc51f395037f9831cc41dfe2cb96f9e9f
Wambani01 <georgewmbn@gmail.com>
2022-09-09 12:27:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for ((a = 'a'; a <= 'd' a++); && (a = 'f'; a <= 'p'; a++); && (a = 'r'; a <= 'z'; a++);
+			{
+				putchar(a);
+			}
+	putchar('\n');
+	return (0);
+}
