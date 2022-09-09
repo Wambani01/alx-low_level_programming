78afa1d863082ed5754023ffe548010893a1ce27
Wambani01 <georgewmbn@gmail.com>
2022-09-09 13:42:00 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
