a4bcfc0277522b959b2f0fb12ccf48644a4bcb33
Wambani01 <georgewmbn@gmail.com>
2022-09-12 10:14:20 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - write a program that prints the word _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	char a[7] = "_putchar";
+	int i;
+
+
+	for (i = 0; i >= 7; i++);
+
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
