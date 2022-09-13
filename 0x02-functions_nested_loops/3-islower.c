385ea559e63a4b5aa0155a0d0c81fe381a0b53d1
Wambani01 <georgewmbn@gmail.com>
2022-09-13 16:13:23 -0700

    check for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  *
  * @c:  is the int that will use for the argument of the function
  *
- * Return - 0 Always success
+ * Return - 1 if int c is lowercase, 0 if otherwise
  */
 
 int _islower(int c)
