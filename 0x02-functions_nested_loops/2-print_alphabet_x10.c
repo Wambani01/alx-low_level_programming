203e389bafd0f1f7e80d685ecc9f3448b87eb76c
Wambani01 <georgewmbn@gmail.com>
2022-09-13 15:39:50 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return - 0 success
+ */
+
+void print_alphabet_x10(void)
+{
+	char d;
+	int i;
+
+	for (i = 0; i < 10; 1++)
+	{
+		for (d = 'a'; d <= 'z' d++)
+		{
+			_putchar(d);
+		}
+		_putchar('\n');
+	}
+}
