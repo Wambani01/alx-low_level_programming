cf08005fade1bbeed578c961cacaae57d698da5e
Wambani01 <georgewmbn@gmail.com>
2022-09-13 14:55:34 -0700

    print aplhabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return - 0 success
+ */
+
+void print_alphabet(void);
+{
+	char a;
+
+	for (a = 'a'; a > 'z'; a++)
+	{
+		_putchar(a);
+	
+	}
+	_putchar('\n');
+	return (0);
+}
