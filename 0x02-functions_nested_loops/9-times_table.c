04e5fcda04397ea85febd16ee410c4f9126b2991
Wambani01 <georgewmbn@gmail.com>
2022-09-14 11:01:45 -0700

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,17 +10,38 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, product;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(',');
-			_putchar('\n');
+			product = i * j;
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			if (y == 0)
+			{
+				_putchar('0');
+
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if ((product < 10) && (y != 0))
+			{
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+
 		}
+		_putchar('\n');
 	}
+
 }
