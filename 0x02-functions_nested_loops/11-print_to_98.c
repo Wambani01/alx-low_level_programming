12fa34d004350630894e4631df13b8dbd65913d0
Wambani01 <georgewmbn@gmail.com>
2022-09-14 12:19:52 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
@@ -21,10 +21,16 @@ void print_to_98(int n)
 	}
 	else if (n < 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
+
+	}
+
+	else if (n == 98)
+	{
+		printf("%d", n);
 	}
 	printf("\n");
 }
