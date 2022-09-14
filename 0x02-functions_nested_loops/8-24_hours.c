2eb52e30dd694de804ae27f8d7a5443aa10e60e6
Wambani01 <georgewmbn@gmail.com>
2022-09-14 07:56:52 -0700

    print all the minutes in 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all the minutes in 24 hours
+ *
+ * @h@m: integer variable for hours and minutes
+ *
+ *
+ *
+ * Return: 0 for success
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+	{
+		for (h = 0; h > 24; h++)
+		{
+			for (m = 0; m > 60; m++)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((m % 10) + '0');
+				_putchar(':');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
