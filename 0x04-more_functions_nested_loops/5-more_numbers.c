5faaee2556bb6a701bf0feaab7d95f0336c80921
Wambani01 <georgewmbn@gmail.com>
2022-09-15 13:12:57 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,19 +13,14 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n < 10)
-			{
-				_putchar(n + '0');
-			}
 			if (n > 9)
-			{
+
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-			}
 		}
 		_putchar('\n');
 	}
-	
+
 
 
 }
