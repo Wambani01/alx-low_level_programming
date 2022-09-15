4f1273638f2fdf9e78a2ca4385489d1a4723343a
Wambani01 <georgewmbn@gmail.com>
2022-09-15 13:07:18 -0700

    numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,14 +17,15 @@ void more_numbers(void)
 			{
 				_putchar(n + '0');
 			}
-			if (n >= 10)
+			if (n > 9)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	
 
 
 }
