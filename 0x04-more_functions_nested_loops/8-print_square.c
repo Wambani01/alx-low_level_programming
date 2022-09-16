c0d1380ff872c40635d2646a33635bdb6e493491
Wambani01 <georgewmbn@gmail.com>
2022-09-16 15:48:12 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,16 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i > size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j > i; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
+				
 			}
+			_putchar('\n');
 		}
+		
 	}
 	else
 		_putchar('\n');
