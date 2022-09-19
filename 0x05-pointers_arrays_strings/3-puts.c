734ae6defcc67c2409f88257af43f8decb0a4d30
Wambani01 <georgewmbn@gmail.com>
2022-09-19 07:58:03 -0700

    print string to stdo

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdo
+ * @str: string being printed
+ * Return: 0 success
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(stri[i]);
+		i++;
+
+	}
+	_putchar('\n');
+}
