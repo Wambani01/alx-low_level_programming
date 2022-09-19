7f0011cd80ca5312162d07250f72f0d8bab21640
Wambani01 <georgewmbn@gmail.com>
2022-09-19 16:34:33 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcpy - copy one string to another
+ * @dest: parameter
+ * @src: parameter
+ * Return: 0 success
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		src[i] = dest[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
