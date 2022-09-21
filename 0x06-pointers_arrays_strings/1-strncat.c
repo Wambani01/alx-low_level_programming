b05e5984b5ffca1afeb27eb66854e1a02bd08bc3
Wambani01 <georgewmbn@gmail.com>
2022-09-21 14:01:21 -0700

    join strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int len1 = strlen(dest);
 
-	for (i = 0; i <= n && src[i] != 0; i++)
+	for (i = 0; i < n && src[i] != 0; i++)
 	{
 		dest[len1 + i] = src[i];
 	}
