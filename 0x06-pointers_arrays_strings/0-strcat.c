9a3a0eb87a22c2043b7ade9c4d32ad6705e40312
Wambani01 <georgewmbn@gmail.com>
2022-09-21 10:08:23 -0700

    join two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: sourc string
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i;
+	int len1 = strlen(dest);
+	int len2 = strlen(src);
+
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[len2];
+	}
+	return (dest);
+}
