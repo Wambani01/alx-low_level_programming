adbb7d6dd8f3cbbfc83ae4e2d63ecc5e08078ea0
Wambani01 <georgewmbn@gmail.com>
2022-09-22 10:13:44 -0700

    comapre strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int value;
-	i = 0;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-		{
-			value = 0;
-		}
-		if (s1[i] > s2[i])
-		{
-			value = 1;
-		}
-		if (s2[i] > s1[i])
-		{
-			value = -1;
-		}
+		value = (s1[i] - s2[i];
 		i++;
 	}
 	return (value);
