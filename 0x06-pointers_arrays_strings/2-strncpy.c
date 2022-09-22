896d02b72e6f700b34d55b586fceb4e441d9b4ab
Wambani01 <georgewmbn@gmail.com>
2022-09-21 19:16:22 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,9 +8,21 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int len1 = strlen(dest);
+	int len2 = strlen(src);
+
+	if (n < len2)
+	{
 	for (n = 0; src[n] != '\0'; n++)
 	{
 		dest[n] = src[n];
+	}
+	return (dest);
+	}
+	else if (n >= len2)
+	{
+		for (n = 0; n <= len2; n++)
+			dest[len1 + n] = src[n];
 	}
 	dest[n] = '\0';
 	return (dest);
