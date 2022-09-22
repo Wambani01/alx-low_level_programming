6bc4f97e80c3ccf559bcd661d55120b473bf85f7
Wambani01 <georgewmbn@gmail.com>
2022-09-22 13:23:08 -0700

    chnage string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_ - change lowercase to upper
+ * @s: string to make uppercase
+ * Return: uppercase letters
+ */
+char *string_toupper(char *s);
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
