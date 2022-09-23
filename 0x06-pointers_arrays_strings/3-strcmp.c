1aafe11633031e789be073923544e3d00e3d91b9
Wambani01 <georgewmbn@gmail.com>
2022-09-23 12:39:34 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _strcmp - compare two strings
  * @s1: string 1
@@ -8,7 +8,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0; 
+	int i = 0;
 
 	while ((s1[i] - s2[i] == 0) && (s1[i] != '\0' || s2[i] != '\0'))
 	{
