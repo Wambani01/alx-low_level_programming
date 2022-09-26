49c47f309462bb876dfeb9eddaf99de3de2dd5bf
Wambani01 <georgewmbn@gmail.com>
2022-09-26 04:05:39 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - memset
+ * @s: string array
+ * @b: char being assigned to memory
+ * @n: number of  bytes being used
+ * Return: Array pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = 'b';
+	}
+	retun(s);
+}
