b404bd94158ad2c8f68a6ecd05ac3f782497c544
Wambani01 <georgewmbn@gmail.com>
2022-09-26 15:25:26 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,13 +5,13 @@
  * @accept: reference string
  * Return: pointer to first occurence
  */
-char *_strpbrk(char *s, char *accept);\
+char *_strpbrk(char *s, char *accept)
 {
-	int i; j;
+	int i, j;
 
 	for (i = 0; s[i] != 0; i++)
 	{
-		fo (j = 0; accept[j] != 0; j++)
+		for (j = 0; accept[j] != 0; j++)
 		{
 			if (s[i] == accept[j])
 			{
@@ -19,6 +19,6 @@ char *_strpbrk(char *s, char *accept);\
 			}
 		}
 	}
-	return (s[i]);
+	return (s);
 
 }
