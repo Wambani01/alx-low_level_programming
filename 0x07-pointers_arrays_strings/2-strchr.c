c1dde412f8c86ec1c9987063a9318e42edeca38d
Wambani01 <georgewmbn@gmail.com>
2022-09-26 06:56:29 -0700

    strncr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-		{
-			char *p = s[i];
-			return (p);
-		}
+		if (*s == c)
+			return (str);
+		s++;
 	}
 	return (0);
 }
