baefecd61926cc5ffee84596aa72d88b72d07f74
Wambani01 <georgewmbn@gmail.com>
2022-09-27 05:32:25 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strstr - find first occurence of a substring
  * @haystack: string being checked
@@ -9,14 +10,14 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	len1 = strlen(haystack);
-	len2 = strlen(needle);
+	int len1 = strlen(haystack);
+	int len2 = strlen(needle);
 
 	for (i = 0; i < len1; i++)
 	{
 		for (j = 0; j < len2; j++)
 		{
-			if (haystack[i + j] != needle[i]
+			if (haystack[i + j] != needle[i])
 					break;
 			if (j == len2)
 				return (haystack);
