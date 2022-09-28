546966f5424e639685a6b6bf3b6758b271ca456d
Wambani01 <georgewmbn@gmail.com>
2022-09-28 13:12:46 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != 0)
+	if (*s == 0)
 	{
-		_strlen_recursion(*s);
-		s++;
-		return (s);
+		return (0);
 	}
 	else
 	{
-		return (0);
+		return (1 + _strlen_recursion(s+1));
 	}
 }
