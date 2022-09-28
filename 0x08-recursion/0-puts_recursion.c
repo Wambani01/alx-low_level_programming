b4b7154fd9cce32422004401dd3adb0aba50f0c5
Wambani01 <georgewmbn@gmail.com>
2022-09-28 10:33:04 -0700

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion- a function that prints a strinf followed by new line
+ * @s: string being printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	_putchar('\n');
+}
