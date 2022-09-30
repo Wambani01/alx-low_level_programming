8e79dfa4e25fc41a2b1eda84811f56e93a646c66
Wambani01 <georgewmbn@gmail.com>
2022-09-30 10:30:54 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - print all the arguments
+ * @argc: argument counter
+ * @argv: string array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%d\n", *argv);
+	}
+	return (0);
+}
