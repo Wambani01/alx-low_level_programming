5453e5c0ef11ff2d24a249a17d13433012b05d3a
Wambani01 <georgewmbn@gmail.com>
2022-09-30 14:34:00 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc - 1; i++)
 	{
-		if (atoi(argv[i]) != isdigit(atoi(argv[i])))
+		if (!isdigit(atoi(argv[i])))
 		{
 			printf("Error\n");
 		}
