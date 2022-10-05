9bdd8774f285db70cd2e808bea31b0b0936d935e
Wambani01 <georgewmbn@gmail.com>
2022-10-05 06:54:41 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,6 +9,8 @@
 int **alloc_grid(int width, int height)
 {
 	int **matrix;
+	int i = 0;
+	int j = 0;
 
 	matrix = malloc(sizeof(int *) * height);
 
@@ -17,12 +19,19 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
-		matrix[i] = malloc(sizeof(int *) * width)
+		matrix[i] = malloc(sizeof(int *) * width);
 		if (width == NULL)
 		{
 			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] = 0;
 		}
 	}
 	return (matrix);
