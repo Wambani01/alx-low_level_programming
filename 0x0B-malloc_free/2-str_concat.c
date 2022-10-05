8c3fd663a5737473c46ab79762718fc0aae2badd
Wambani01 <georgewmbn@gmail.com>
2022-10-05 12:26:50 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,30 +10,34 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0;
-	int s1_length = strlen(s1);
-	int s2_length = strlen(s2);
-	int n = s1_length + s2_length + 1;
+	int s1_length;
+	int s2_length;
+	int n;
 	char *dest;
+
+	if (s1 === NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		s1_length = strlen(s1);
+		s2_length = strlen(s2);
+		n = s1_length + s2_length + 1;
+	}
 
 	dest = malloc(sizeof(char) * n);
 	if (dest == NULL)
 	{
 		return (NULL);
 	}
-	if (s1 == NULL || s2 == NULL)
+	for (i = 0; i < s1_length; i++)
 	{
-		return (NULL);
+		dest[i] = s1[i];
 	}
-	else
+	for (i = 0; i < s2_length + 1; i++)
 	{
-		for (i = 0; i < s1_length; i++)
-		{
-			dest[i] = s1[i];
-		}
-		for (i = 0; i < s2_length + 1; i++)
-		{
-			dest[s1_length + i] = s2[i];
-		}
+		dest[s1_length + i] = s2[i];
 	}
 	return (dest);
 	free(dest);
