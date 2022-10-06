86b5acf60ab0de51a7a24ee693a7f0e97860243e
Wambani01 <georgewmbn@gmail.com>
2022-10-06 15:50:33 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * malloc_checked - assigns memory space
+ * @b: parametr being assigned space
+ * Return: returns void pointer
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
