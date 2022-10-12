8b5000479cdee626c6bea5395be97056b838f339
Wambani01 <georgewmbn@gmail.com>
2022-10-12 02:40:11 -0700

    task 1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
  * print_dog - prints strduct dog
  * @d: structure being printed
@@ -11,11 +12,11 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("Name :(nil)\n");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
-			printf("Name :%s\n", d->name);
+			printf("Name: %s\n", d->name);
 		}
 		printf("Age : %.6f\n", d->age);
 		if (d->owner == NULL)
@@ -24,7 +25,11 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Owner : %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		}
+	}
+	else
+	{
+		return;
 	}
 }
