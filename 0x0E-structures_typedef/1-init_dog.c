e5e3c559dc1ae471ee8fb4086fa63c6637353eef
Wambani01 <georgewmbn@gmail.com>
2022-10-12 01:55:57 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,7 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
