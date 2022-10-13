dd49868a9db62752de7eb0c27d5a7d4d99080a8d
Wambani01 <georgewmbn@gmail.com>
2022-10-13 03:59:51 -0700

    task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
