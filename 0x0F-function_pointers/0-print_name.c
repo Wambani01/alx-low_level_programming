3e201f3433a70bf2545b8ce68449b6ec80bb102a
Wambani01 <georgewmbn@gmail.com>
2022-10-13 03:39:29 -0700

    task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: function pointer
  * Return: nothing
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *name))
 {
 	if (name == NULL || f == NULL)
 	{
