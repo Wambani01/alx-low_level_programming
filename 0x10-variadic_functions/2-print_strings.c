887b95c393bec3ed8c020cb4d1d4a7b744422d02
Wambani01 <georgewmbn@gmail.com>
2022-10-14 06:04:16 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,6 +5,7 @@
  * print_strings - prints strings
  * @n: number of strings
  * @...: optional arguments
+ * @separator: string printed btn strings
  * Return: nothing
  */
 void print_strings(const char *separator, const unsigned int n, ...)
