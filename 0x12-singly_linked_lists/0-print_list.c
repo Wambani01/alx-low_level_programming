4bad3de738ed74c136013a2034afc7988d1d855d
Wambani01 <georgewmbn@gmail.com>
2022-10-25 03:28:58 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,8 +3,7 @@
 #include "lists.h"
 /**
  * print_list - print elements of a list
- * @list_t - list structure
- * @h - pointer to the list
+ * @h: pointer to the list
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
@@ -28,7 +27,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str); 
+			printf("[%u] %s\n", h->len, h->str);
 		}
 	}
 	return (count);
