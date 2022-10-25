691bbe865ac40df54f8e9f2d6e220e67397c1222
Wambani01 <georgewmbn@gmail.com>
2022-10-25 09:19:08 -0700

    task 1

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - This function frees a list_t list
+ * @head: The list to free
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
