195c75f9ad387ae7a19e87ebb0ee95840f6eca1e
Wambani01 <georgewmbn@gmail.com>
2022-10-25 04:33:33 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - number of elements in a linked list
+ * @h: pointer to structure
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	if (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			count++;
+		}
+		if (h->len)
+		{
+			count++;
+		}
+	}
+	return (count);
+}
