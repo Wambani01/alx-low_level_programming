4b0b6c5fc4f96324071c616760825a12a3d660ab
Wambani01 <georgewmbn@gmail.com>
2022-10-25 04:04:00 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,25 +9,29 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
+	const list_t *ptr = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
 
 	if (h != NULL)
 	{
-		const list_t *ptr = NULL;
-
 		ptr = h;
 
 		while (ptr != NULL)
 		{
 			count++;
-			ptr = ptr->next;
-		}
-		if (h->str == NULL)
+		if (ptr->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		}
+			ptr = ptr->next;
 		}
 	}
 	return (count);
