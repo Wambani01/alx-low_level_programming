6aec68c67b46b000682c5cdc44fe8d72f8c4d1cc
Wambani01 <georgewmbn@gmail.com>
2022-10-27 05:25:30 -0700

    task 1

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,13 +8,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	while (h != NULL)
+	while (head != NULL)
 	{
 		tmp = head;
 
 		head = head->next;
 
-		free(tmp->n);
 		free(tmp);
 	}
 }
