9b4613b154e5eb545f13d84abbca70f4e32a51ee
Wambani01 <georgewmbn@gmail.com>
2022-10-27 01:53:46 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->n);
+		free(tmp);
+	}
+	head = NULL;
+}
