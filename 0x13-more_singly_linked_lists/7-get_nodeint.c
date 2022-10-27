5a96306bbe347f5bb03ef54de0ab58996b4911d7
Wambani01 <georgewmbn@gmail.com>
2022-10-27 07:07:24 -0700

    task 8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,11 +3,12 @@
  * get_nodeint_at_index - gets index at nth node
  * @head: head pointer
  * @index: index of nth node
+ * Return: pointer to the to the nth node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	if (head == NULL)
 	{
 		return (NULL);
