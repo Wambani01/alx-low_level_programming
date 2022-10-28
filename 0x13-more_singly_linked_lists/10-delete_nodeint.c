05b84ecc38e991aa8d7fde55c00a22089af9b613
Wambani01 <georgewmbn@gmail.com>
2022-10-28 06:33:37 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes index at a certain index
+ * @head: pointer to the list
+ * @index: index being dleted at
+ * Return: -1 on failure and 1 0n sucess
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current;
+	listint_t *previous;
+	unsigned int count;
+
+	if (head == NULL)
+	{
+		return (-1);
+	}
+
+	current = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		current = NULL;
+		return (1);
+	}
+
+	count = 0;
+
+	while (count != index)
+	{
+		previous = current;
+		current = current->next;
+
+		if (current == NULL)
+		{
+			return (-1);
+		}
+		count++;
+	}
+
+	previous-> = current->next;
+	free(current);
+	current = NULL;
+	return (1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,35 +10,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int counter = 0;
 	listint_t *tmp;
 	listint_t *tmp1;
-
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-
 	tmp = *head;
-
 	if (tmp == NULL)
 	{
 		return (NULL);
 	}
-
 	tmp1 = malloc(sizeof(listint_t));
-
 	if (tmp1 == NULL)
 	{
 		return (NULL);
 	}
-
 	tmp1->n = n;
-
 	if (idx == 0)
 	{
 		tmp1->next = *head;
 		*head = tmp1;
 		return (tmp1);
 	}
-
 	while (counter != idx - 1)
 	{
 		tmp = tmp->next;
@@ -49,7 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		counter++;
 	}
-
 	tmp1->next = tmp->next;
 	tmp->next = tmp1;
 
