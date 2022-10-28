965f46b416b7f78a812fe17c2b09a5feba01ae71
Wambani01 <georgewmbn@gmail.com>
2022-10-28 06:36:41 -0700

    task 1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -45,7 +45,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		count++;
 	}
 
-	previous-> = current->next;
+	previous->next = current->next;
 	free(current);
 	current = NULL;
 	return (1);
