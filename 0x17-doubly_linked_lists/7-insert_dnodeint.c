df7bebda1e1dde9c99d6bd6033f2e55c233e96f6
Wambani01 <georgewmbn@gmail.com>
2022-12-08 11:36:21 -0800

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts node at index
+ * @idx: index
+ * @n: node
+ * @h: list head
+ * Return:the new node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp, *new;
