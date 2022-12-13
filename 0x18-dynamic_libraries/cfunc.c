4bfac9be0415a7425eff6529aae0b9c5c482151a
Wambani01 <georgewmbn@gmail.com>
2022-12-13 03:30:25 -0800

    cpython

---
+++ 0x18-dynamic_libraries/cfunc.c
@@ -0,0 +1,51 @@
+#include "f.h"
+/**
+ * add - adds two numbers
+ * @a: variable 1
+ * @b: variable 2
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * mul - multiply two digits
+ * @a: variable 1
+ * @b: variable 2
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * sub - substracts two ints
+ * @a: variable 1
+ * @b: variable 2
+ * Return: value after subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - divides two digits
+ * @a: variable 1
+ * @b: variable 2
+ * Return: value after division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus of two digits
+ * @a: variable 1
+ * @b: variable 2
+ * Return: value after modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
