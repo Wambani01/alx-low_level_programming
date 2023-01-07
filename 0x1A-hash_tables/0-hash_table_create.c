373485d03392c12ea2b84aea04b2e2042b084463
Wambani01 <georgewmbn@gmail.com>
2023-01-07 04:27:30 -0800

    created hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates hash table
+ * @size: size of the table
+ * Return: pointer to the table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
+
